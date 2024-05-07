e8cd0a887cd8ac1c4069b5054aad851806bfd397
tiagomb <tmb22@inf.ufpr.br>
2024-05-07 18:48:50 -0300

    first try on sequences, timeouts, and video listing

--- t1/buffer.c
+++ t1/buffer.c
@@ -5,7 +5,14 @@
 #include <stdio.h>
 #include <errno.h>
 #include <sys/socket.h>
+#include <sys/time.h>
 #include "crc.h"
+
+long long int timestamp(){
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    return tv.tv_sec * 1000 + tv.tv_usec/1000;
+}
 
 int envia_buffer(int soquete, unsigned int sequencia, unsigned int tipo, unsigned char* dados, unsigned int tamanho){
     unsigned char *buffer = (unsigned char*) malloc(sizeof(protocolo_t));
@@ -31,18 +38,30 @@ int envia_buffer(int soquete, unsigned int sequencia, unsigned int tipo, unsigne
     return 0;
 }
 
+int buffer_eh_valido(protocolo_t *pacote){
+    return pacote->marcador == 126;
+}
+
+int recebe_msg(int soquete, unsigned char *buffer){
+    long long int comeco = timestamp();
+    int bytes_recebidos = 0;
+    do{
+        bytes_recebidos = recv(soquete, buffer, sizeof(protocolo_t), 0);
+        if (buffer_eh_valido((protocolo_t*) buffer)){
+            return bytes_recebidos;
+        }
+    } while (timestamp() - comeco <= TIMEOUT);
+    return -1;
+}
+
 int recebe_buffer(int soquete, protocolo_t *pacote){
     unsigned char *buffer = (unsigned char*) malloc(sizeof(protocolo_t));
     protocolo_t *pacote_recebido = (protocolo_t*) buffer;
-    int recebido = recv(soquete, buffer, sizeof(protocolo_t), 0);
+    int recebido = recebe_msg(soquete, buffer);
     if (recebido == -1) {
-        fprintf(stderr, "Erro ao receber pacote: %s\n", strerror(errno));
+        fprintf(stderr, "Timeout %s\n", strerror(errno));
         free(buffer);
         return ERRO;
-    }
-    if (pacote_recebido->marcador != 126) {
-        free(buffer);
-        return IGNORAR;
     }
     if (recebido == 0 || calculaCRC(&buffer[1], sizeof(protocolo_t) - 1, tabela_crc) != 0){
         free(buffer);
