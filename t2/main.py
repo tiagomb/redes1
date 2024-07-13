40a82df282daf2e74e413911a7ddfb9acb8b31a6
tiagomb <tmb22@inf.ufpr.br>
2024-07-13 19:08:06 -0300

    Import connection

--- t2/main.py
+++ t2/main.py
@@ -1,30 +1,30 @@
 from game import Deck, Round
 import config as cfg
-from connection import send_data, send_token, receive_packet
+import connection as con
 
 def main():
     maquina = int(input("Digite o número da máquina(0 a 3): "))
     config = cfg.get_config(maquina)
     if maquina == 0:
         rodada = Round(1, 4)
-        send_data(config, rodada.shackle, 'shackle', 3)
-        packet = receive_packet(config)
+        con.send_data(config, rodada.shackle, 'shackle', 3)
+        packet = con.receive_packet(config)
         print (rodada.hands[0])
         if packet.origin == 0 and packet.confirmation == True:
             for i in range(1, 4):
-                send_data(config, rodada.hands[i], 'hand', i)
-                packet = receive_packet(config)
+                con.send_data(config, rodada.hands[i], 'hand', i)
+                packet = con.receive_packet(config)
                 if packet.origin == 0 and packet.confirmation == True:
                     print(f"Recebeu")
-        send_token(config)
+        con.send_token(config)
     while True:
-        packet = receive_packet(config)
+        packet = con.receive_packet(config)
         if packet.destiny == maquina and packet.kind == 'hand':
             packet.confirmation = True
-            retransmit(packet, config)
+            con.retransmit(packet, config)
             print(f"Recebeu {packet.data}")
         else:
-            retransmit(packet, config)
+            con.retransmit(packet, config)
     
 
 if __name__ == '__main__':
