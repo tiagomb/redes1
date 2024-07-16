45f4aa2164de419f2cd6d9701ef09d44aa7f2f1a
tiagomb <tmb22@inf.ufpr.br>
2024-07-16 12:48:45 -0300

    Remove unused code, separate logic into functions

--- t2/game.py
+++ t2/game.py
@@ -7,7 +7,7 @@ class Card:
         self.weight = weight
 
     def __repr__(self):
-        return f"{self.num} {self.suit} {self.weight}"
+        return f"{self.num} {self.suit}"
 
 class Deck:
     nums = ["4", "5", "6", "7", "J", "Q", "K", "A", "2", "3"]
@@ -37,6 +37,17 @@ class Game:
         self.handSize = 1
         self.lifes = 7
 
+    def end_round(self, hand, round, machine, winner, alives):
+        if winner == machine:
+            hand.points += 1
+        round.plays = 0
+        self.lifes -= abs(hand.bet - hand.points)
+        self.alives = alives
+        hand.reset()
+        if self.lifes == 0:
+            self.alives.remove(machine)
+        print ("Vidas: ", self.lifes)
+
 class Hand:
     def __init__(self):
         self.points = 0
@@ -45,6 +56,36 @@ class Hand:
         self.bet = None
         self.bet_sum = 0
         self.bet_quantity = 0
+
+    def reset(self):
+        self.points = 0
+        self.cards = []
+        self.shackle = None
+        self.bet = None
+        self.bet_sum = 0
+        self.bet_quantity = 0
+
+    def update_bets(self, bet):
+        self.bet_quantity += 1
+        self.bet_sum += 
+        print ("Apostas até agora: ", self.bet_sum)
+
+    def update_cards(self, hand, game):
+        self.cards = hand
+        game.handSize = len(self.cards)
+        print ("Cartas: ", self.cards)
+
+    def update_shackle(self, shackle):
+        self.shackle = shackle
+        print ("Manilha: ", self.shackle)
+
+    def place_bet(self, bet, alives):
+        self.bet = int(input("Digite a aposta: "))
+        if self.bet_quantity == len(alives) - 1:
+            while (self.bet + self.bet_sum == len(self.cards)):
+                self.bet = int(input("A soma das apostas não pode ser igual ao número de cartas, escolha novamente: "))
+        self.bet_sum += self.bet
+        self.bet_quantity += 1
         
 class Deal:
     def __init__(self, num, players):
@@ -52,6 +93,14 @@ class Deal:
         self.deck.shuffle()
         self.shackle = self.deck.get_shackle()
         self.hands = self.deck.get_hands(num ,players)
+
+    def distribute_hands(self, player, config, vivos):
+        index = vivos.index(player)
+        for i in range(index+1, index+len(vivos)):
+            con.send_data(config, self.hands[i%len(vivos)], 'hand', vivos[i%len(vivos)])
+            packet = con.receive_packet(config)
+            if packet.origin == player and packet.confirmation == True:
+                print ("recebeu mão")
 
 class Round:
     def __init__(self):
@@ -63,6 +112,21 @@ class Round:
         if self.winning_card is None or card.weight > self.winning_card.weight:
             self.winning_card = card
             self.winning_player = player
+        self.plays += 1
+        print ("Carta jogada: ", card)
+
+    def check_winner(self, winner, hand, machine):
+        if winner = machine:
+            hand.points += 1
+        print ("Vencedor da rodada: ", winner)
+        self.plays = 0
+
+    def play_card(self, cards):
+        print ("Suas cartas: ")
+        for i, card in enumerate(cards):
+            print (f"{i} - {card}")
+        card = int(input("Digite o número da carta que deseja jogar: "))
+        return cards.pop(card)
 
         
     def __repr__(self):
--- t2/main.py
+++ t2/main.py
@@ -2,87 +2,6 @@ from game import Deck, Round, Game, Hand, Deal
 import config as cfg
 import connection as con
 
-# def play_hand(jogador, config, maquina, rodada):
-#     while jogador.rounds:
-#         packet = con.receive_packet(config)
-#         if packet.origin != maquina and packet.kind != 'token':
-#             if packet.kind == 'bet':
-#                 jogador.bet_sum += packet.data
-#                 jogador.bet_quantity+=1
-#             con.retransmit(packet, config)
-#         elif packet.origin == maquina:
-#             con.send_token(config)
-#         else:
-#             if jogador.bet == None:
-#                 if jogador.bet_quantity == 3:
-#                     jogador.bet = int(input("Digite a aposta: "))
-#                     while (jogador.bet + jogador.bet_sum == len(jogador.hand)):
-#                         jogador.bet = int(input("A soma das apostas não pode ser igual ao número de cartas, escolha novamente: "))
-#                     jogador.bet_sum += jogador.bet
-#                 else:
-#                     jogador.bet = int(input("Digite a aposta: "))
-#                     jogador.bet_sum += jogador.bet
-#                     jogador.bet_quantity += 1
-#                 con.send_data(config, jogador.bet, 'bet', (maquina + 3) % 4)
-#             else:
-#                 play = int(input("Selecione a carta que deseja jogar: "))
-#                 rodada.set_card(maquina, jogador.hand[play])
-
-
-# def main():
-#     maquina = int(input("Digite o número da máquina(0 a 3): "))
-#     config = cfg.get_config(maquina)
-#     rodada = Round()
-#     jogador = Player()
-#     mao = Hand(1, 4, 0)
-#     while jogador.alives > 1:
-#         if maquina == mao.dealer:
-#             con.send_data(config, mao.shackle, 'shackle', (maquina + 3) % 4)
-#             jogador.hand = mao.hands[maquina]
-#             jogador.rounds = len(jogador.hand)
-#             packet = con.receive_packet(config)
-#             if packet.origin == maquina and packet.confirmation == True:
-#                 for i in range(4):
-#                     if i != maquina:
-#                         con.send_data(config, mao.hands[i], 'hand', i)
-#                         packet = con.receive_packet(config)
-#                         if packet.origin == maquina and packet.confirmation == True:
-#             con.send_token(config)
-#             play_hand(jogador, config, maquina, rodada)
-#         packet = con.receive_packet(config)
-#         if packet.kind == 'hand':
-#             if packet.destiny != maquina:
-#                 con.retransmit(packet, config)
-#             else:
-#                 packet.confirmation = True
-#                 jogador.hand = packet.data
-#                 jogador.rounds = len(jogador.hand)
-#                 con.retransmit(packet, config)
-#                 play_hand(jogador, config, maquina, rodada)            
-#         else:
-#             jogador.shackle = packet.data
-#             if packet.destiny == maquina:
-#                 packet.confirmation = True
-#             con.retransmit(packet, config)
-#         print(packet.kind)
-#         print(packet.data)
-
-def distribute_hands(maquina, config, carteador, vivos):
-    index = vivos.index(maquina)
-    for i in range(index+1, index+len(vivos)):
-        con.send_data(config, carteador.hands[i%len(vivos)], 'hand', vivos[i%len(vivos)])
-        packet = con.receive_packet(config)
-        if packet.origin == maquina and packet.confirmation == True:
-            print ("recebeu mão")
-
-def reset_hand(mao):
-    mao.points = 0
-    mao.cards = []
-    mao.shackle = None
-    mao.bet = None
-    mao.bet_sum = 0
-    mao.bet_quantity = 0
-
 def main():
     maquina = int(input("Digite o número da máquina(0 a 3): "))
     config = cfg.get_config(maquina)
@@ -92,106 +11,62 @@ def main():
     if maquina == 0:
         carteador = Deal(1, 4)
         con.send_data(config, carteador.shackle, 'shackle', (maquina + 3) % 4)
-        mao.shackle = carteador.shackle
-        print("Shackle: ", mao.shackle)
-        mao.cards = carteador.hands[maquina]
-        print("Cartas: ", mao.cards)
+        mao.update_shackle(carteador.shackle)
+        mao.update_cards(carteador.hands[maquina], jogo)
     while len(jogo.alives) > 1:
         packet = con.receive_packet(config)
         if jogo.lifes == 0:
             if packet.destiny == maquina:
                 packet.confirmation = True
             con.retransmit(packet, config)
-        elif packet.origin != maquina and packet.kind != 'token':
+        elif packet.origin != maquina and packet.kind != 'token': # Case where you update and sync your info and retransmit the packet
             if packet.kind == 'shackle':
-                mao.shackle = packet.data
-                print ("Shackle: ", mao.shackle)
+                mao.update_shackle(packet.data)
             elif packet.kind == 'hand':
                 if packet.destiny == maquina:
-                    mao.cards = packet.data
-                    jogo.handSize = len(mao.cards)
-                    print("Cartas: ", mao.cards)
+                    mao.update_cards(packet.data, jogo)
             elif packet.kind == 'bet':
-                mao.bet_sum += packet.data
-                mao.bet_quantity+=1
-                print ("Apostas até agora: ", mao.bet_sum)
+                mao.update_bets(packet.data)
             elif packet.kind == 'play':
                 rodada.set_card(packet.origin, packet.data)
-                rodada.plays += 1
-                print ("Carta jogada: ", packet.data)
             elif packet.kind == 'win':
-                if packet.data == maquina:
-                    mao.points += 1
-                print ("Vencedor da rodada: ", packet.data)
-                rodada.plays = 0
+                rodada.check_winner(packet.data, mao, maquina)
             elif packet.kind == 'update':
-                if packet.data[1] == maquina:
-                    mao.points += 1
-                rodada.plays = 0
-                jogo.lifes -= abs(mao.bet - mao.points)
-                jogo.alives = packet.data[0]
-                reset_hand(mao)
-                if jogo.lifes == 0:
-                    jogo.alives.remove(maquina)
+                jogo.end_round(mao, rodada, maquina, packet.data[1], packet.data[0])
                 packet.data[0] = jogo.alives
-                print ("Vidas: ", jogo.lifes)
             if packet.destiny == maquina:
                 packet.confirmation = True
             con.retransmit(packet, config)
-        elif packet.origin == maquina:
-            if packet.kind == 'shackle':
-                distribute_hands(maquina, config, carteador, jogo.alives)
+        elif packet.origin == maquina: # Case where you received your own packet and have the token, usually you just pass the token
+            if packet.kind == 'shackle': # If you have the token and received a shackle, it means you are the dealer and need to distribute the hands
+                carteador.distribute_hands(maquina, config, jogo.alives)
                 con.send_token(config)
-            elif packet.kind == 'update':
+            elif packet.kind == 'update': #Means the hand is over and you need to become the new dealer
                 jogo.alives = packet.data[0]
                 if len(jogo.alives) > 1:
                     if jogo.handSize == 7:
                         carteador = Deal(jogo.handSize, len(jogo.alives))
                     else:
                         carteador = Deal(jogo.handSize+1, len(jogo.alives))
-                    mao.shackle = carteador.shackle
-                    print ("Shackle: ", mao.shackle)
+                    mao.update_shackle(carteador.shackle)
                     con.send_data(config, mao.shackle, 'shackle', (maquina + 3) % 4)
-                    mao.cards = carteador.hands[maquina]
-                    print ("Cartas: ", mao.cards)
-                    jogo.handSize = len(mao.cards)
+                    mao.update_cards(carteador.hands[maquina], jogo)
             else:
                 con.send_token(config)
-        else:
-            if mao.bet == None:
-                if mao.bet_quantity == len(jogo.alives) - 1:
-                    mao.bet = int(input("Digite a aposta: "))
-                    while (mao.bet + mao.bet_sum == len(mao.cards)):
-                        mao.bet = int(input("A soma das apostas não pode ser igual ao número de cartas, escolha novamente: "))
-                    mao.bet_sum += mao.bet
-                else:
-                    mao.bet = int(input("Digite a aposta: "))
-                    mao.bet_sum += mao.bet
-                    mao.bet_quantity += 1
+        else: # Case where you just received the token and hasn't sent any packet yet
+            if mao.bet == None: # If you haven't bet yet, you need to bet
+                mao.place_bet(mao.bet, len(jogo.alives))
                 con.send_data(config, mao.bet, 'bet', (maquina + 3) % 4)
-            elif len(mao.cards) > 0:
-                if rodada.plays != len(jogo.alives):
-                    play = int(input("Selecione a carta que deseja jogar: "))
-                    card = mao.cards.pop(play)
+            elif len(mao.cards) > 0: # If you have cards, you need to play
+                if rodada.plays != len(jogo.alives): # If it's not the last play of the round, you need to play
+                    card = rodada.play_card(mao.cards)
                     con.send_data(config, card, 'play', (maquina + 3) % 4)
                     rodada.set_card(maquina, card)
-                    rodada.plays += 1
-                else:
-                    if rodada.winning_player == maquina:
-                        mao.points += 1
-                    rodada.plays = 0
-                    print("Vencedor da rodada: ", rodada.winning_player)
+                else: # Everybody played, you need to check the winner
+                    rodada.check_winner(rodada.winning_player, mao, maquina)
                     con.send_data(config, rodada.winning_player, 'win', (maquina + 3) % 4)
-            else:
-                if rodada.winning_player == maquina:
-                    mao.points += 1
-                rodada.plays = 0
-                print ("Vencedor da rodada: ", rodada.winning_player)
-                jogo.lifes -= abs(mao.bet - mao.points)
-                print ("Vidas: ", jogo.lifes)
-                reset_hand(mao)
-                if jogo.lifes == 0:
-                    jogo.alives.remove(maquina)
+            else: # If you don't have cards, the hand is over and you need to update the game
+                jogo.end_round(mao, rodada, maquina, rodada.winning_player, jogo.alives)
                 con.send_data(config, [jogo.alives, rodada.winning_player], 'update', (maquina + 3) % 4)
     if jogo.lifes == 0:
         print("Você perdeu!")
